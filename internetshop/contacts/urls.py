de9b2f29838163271aaa0af93e3a2e337963c7c4
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2020-04-20 11:40:45 +0300

    Internet Shop - Stage 01

---
+++ internetshop/contacts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'contacts'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+]
---
+++ internetshop/home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+# class IndexView(generic.ListView):
+#     template_name = 'index.html'
+#
+#     def get_queryset(self):
+#         """
+#         Return the last five published questions (not including those set to be
+#         published in the future).
+#         """
+#         return ""
