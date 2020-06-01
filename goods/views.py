eedfd5a855d94d7a77ed6b246fc16621086e3712
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2020-06-01 21:50:50 +0300

    Add goods and details - fix

--- goods/views.py
+++ goods/views.py
@@ -1,15 +1,16 @@
 import logging
-from django.views import generic
+from django.views.generic import ListView, DetailView
+
 from .models import Goods
 
 logger = logging.getLogger(__name__)
 
 
-class GoodsView(generic.ListView):
+class GoodsView(ListView):
     template_name = 'product_list.html'
     model = Goods
 
 
-class GoodsPropsView(generic.DetailView):
+class GoodsPropsView(DetailView):
     template_name = 'single-product.html'
     model = Goods
